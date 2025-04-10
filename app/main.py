cbf1e886ba7808f0caae5ac07d99fffc3cda0242
Richard Nguyen <11547131+nguyri@users.noreply.github.com>
2025-04-10 15:59:13 -0600

    Added demo slider and graph

--- app/main.py
+++ app/main.py
@@ -1,3 +1,17 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
 
-st.title("Hello World! from streamlit")
+
+st.title("Hello World! \n from streamlit")
+
+number = st.slider("Pick a number", 0, 100)
+
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+# Generate some random data
+chart_data = pd.DataFrame(np.random.randn(10, 3), columns=["a", "b", "c"])
+
+st.bar_chart(chart_data)
